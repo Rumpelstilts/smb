2c986cb68acfe284be1dbc1a2a434d99967663f3
delorge <wazza.ivan@gmail.com>
2016-02-08 19:11:17 +0300

    added address view in founder list

--- src/app/founder/founder.directive.js
+++ src/app/founder/founder.directive.js
@@ -36,12 +36,13 @@
     vm.edit_founder_address = edit_founder_address
     vm.edit_founder_info = edit_founder_info
     vm.name
+    vm.save_founder_address_changes = save_founder_address_changes
     vm.save_founder_changes = save_founder_changes
     activate()
 
     function activate () {
       refresh_founder_personal_data()
-      refresh_founder_address()
+      vm.address = 'Адрес'
     }
 
     function edit_founder_address () {
@@ -58,25 +59,34 @@
     }
 
     function refresh_founder_address () {
-      vm.address = 'Адрес'
-    }
-
-    function refresh_founder_personal_data () {
-      if (vm.founder.personal_data.last_name.trim() === '' &&
-        vm.founder.personal_data.name.trim() === '' &&
-        vm.founder.personal_data.mid_name.trim() === ''
-      ) {
-        vm.name = 'Имя учредителя'
-      } else {
-        vm.name = vm.founder.personal_data.last_name + ' ' +
-          vm.founder.personal_data.name + ' ' +
-          vm.founder.personal_data.mid_name
+      var a = vm.founder.address
+      vm.address = universal_trim(a.city) + ' ' + universal_trim(a.street) + ' ' + universal_trim(a.buildingType) + ' ' + universal_trim(a.building) + ' ' +
+        universal_trim(a.housingType) + ' ' + universal_trim(a.housing) + ' ' + universal_trim(a.flatType) + ' ' + universal_trim(a.flat)
+      if (vm.address === '') {
+        vm.address = 'Адрес'
       }
     }
 
+    function refresh_founder_personal_data () {
+      vm.name = vm.founder.personal_data.last_name + ' ' +
+        vm.founder.personal_data.name + ' ' +
+        vm.founder.personal_data.mid_name
+      if (vm.name.trim() === '') {
+        vm.name = 'Имя учредителя'
+      }
+    }
+
+    function save_founder_address_changes () {
+      refresh_founder_address()
+      vm.edit_address = !vm.edit_address
+    }
     function save_founder_changes () {
       refresh_founder_personal_data()
       vm.edit_info = !vm.edit_info
+    }
+
+    function universal_trim (val) {
+      return (val) ? val.trim() : ''
     }
   }
 })()
